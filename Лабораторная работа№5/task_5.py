8edafb2dcb0fb0153e03b113eaebaf80175f7966
Lubov123456789 <115494208+Lubov123456789@users.noreply.github.com>
2022-12-17 00:15:02 +0300

    Update task_5.py

--- Лабораторная работа№5/task_5.py
+++ Лабораторная работа№5/task_5.py
@@ -1,10 +1,12 @@
 import random
 import string
-def get_random_password() -> str:
+
+
+def get_random_password(n=8) -> str:
     S = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    i = random.sample(S,8)
-    password = str(i)
+    password = random.sample(S,n)
     return password
+    
 print(get_random_password())
 
 
